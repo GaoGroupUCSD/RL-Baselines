f67b3397bb077efae20c1cb887360d7f4ee9a10e
peiranli <pel047@ucsd.edu>
2019-04-12 14:38:23 -0700

    rollout

--- a2c.py
+++ a2c.py
@@ -61,13 +61,14 @@ actor_network = ActorNetwork(STATE_DIM,64,ACTION_DIM)
 actor_network_optim = torch.optim.Adam(actor_network.parameters(),lr = 0.01)
 
 
-def roll_out():
+def roll_out(sample_nums):
     state = env.reset()
     states = []
     actions = []
     rewards = []
-
-    for step in range(SAMPLE_NUMS):
+    is_done = False
+    final_r = 0
+    for step in range(sample_nums):
         states.append(state)
         log_softmax_action = actor_network(Variable(torch.Tensor([state])))
         softmax_action = torch.exp(log_softmax_action)
@@ -78,11 +79,13 @@ def roll_out():
         rewards.append(reward)
         state = next_state
         if done:
+            is_done = True
             break
+    if not is_done:
+        final_r = value_network(Variable(torch.Tensor([state])))
+    return states,actions,rewards,step,final_r
 
-    return states,actions,rewards,step
-
-def update_network(states, actions, rewards):
+def update_network(states, actions, rewards, final_r):
         actions_var = Variable(FloatTensor(actions).view(-1,ACTION_DIM))
         states_var = Variable(FloatTensor(states).view(-1,STATE_DIM))
         # train actor network
@@ -90,7 +93,7 @@ def update_network(states, actions, rewards):
         log_softmax_actions = actor_network(states_var)
         vs = value_network(states_var).detach()
         # calculate qs
-        qs = Variable(torch.Tensor(discount_reward(rewards,0.99)))
+        qs = Variable(torch.Tensor(discount_reward(rewards,0.99, final_r)))
 
         advantages = qs - vs
         actor_network_loss = - torch.mean(torch.sum(log_softmax_actions*actions_var,1)* advantages)
@@ -107,9 +110,9 @@ def update_network(states, actions, rewards):
         value_network_optim.step()
 
 
-def discount_reward(r, gamma):
+def discount_reward(r, gamma, final_r):
     discounted_r = np.zeros_like(r)
-    running_add = 0
+    running_add = final_r
     for t in reversed(range(0, len(r))):
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
@@ -120,9 +123,9 @@ def main():
     print("reward threshold", env.spec.reward_threshold)
 
     for i_episode in count(1):
-        states,actions,rewards,steps = roll_out()
+        states,actions,rewards,steps,final_r = roll_out(SAMPLE_NUMS)
         running_reward = running_reward * 0.99 + steps * 0.01
-        update_network(states,actions,rewards)
+        update_network(states,actions,rewards,final_r)
         
         if i_episode % 50 == 0:
             print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
