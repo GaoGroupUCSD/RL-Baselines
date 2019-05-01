e2c19c6c4af5c0486581cb3af26b9eb5d984fddf
peiranli <pel047@ucsd.edu>
2019-05-01 10:56:28 -0700

    pg continuous

--- pg_continuous.py
+++ pg_continuous.py
@@ -1,22 +1,37 @@
+import math
+import random
+import gym
+import os
+import numpy as np
+from itertools import count
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
 from torch.autograd import Variable
-from itertools import count
-import numpy as np
-import math
-import random
-import os
-import gym
 
-# init a task generator for data fetching
-env = gym.make('Pendulum-v0')
+from common.multiprocessing_env import SubprocVecEnv
+
+num_envs = 16
+env_name = "Pendulum-v0"
+
+def make_env():
+    def make():
+        env = gym.make(env_name)
+        return env
+
+    return make
+
+envs = [make_env() for i in range(num_envs)]
+envs = SubprocVecEnv(envs)
+
+env = gym.make(env_name)
 
 STATE_DIM = env.observation_space.shape[0]
 ACTION_DIM = env.action_space.shape[0]
 ACTION_MAX = env.action_space.high[0]
-SAMPLE_NUMS = 1000
+SAMPLE_NUMS = 50
 
 FloatTensor = torch.FloatTensor
 LongTensor = torch.LongTensor 
@@ -49,35 +64,45 @@ class Actor(nn.Module):
         return dist
 
 # init actor network
-actor_network = Actor(STATE_DIM,ACTION_DIM,256)
+actor_network = Actor(STATE_DIM,ACTION_DIM,256,0)
 actor_network_optim = torch.optim.Adam(actor_network.parameters(),lr = 3e-4)
 eps = np.finfo(np.float32).eps.item()
 
+def test_env(vis=False):
+    state = env.reset()
+    if vis: env.render()
+    done = False
+    total_reward = 0
+    while not done:
+        state = Variable(torch.Tensor(state))
+        dist = actor_network(state)
+        next_state, reward, done, _ = env.step(dist.sample().cpu().numpy())
+        state = next_state
+        if vis: env.render()
+        total_reward += reward
+    return total_reward
+
 def roll_out(sample_nums):
-    observation = env.reset()
+    observation = envs.reset()
     states = []
     actions = []
     rewards = []
     episode_reward = 0
     entropy = 0
     for _ in range(sample_nums):
-        env.render()
+        #env.render()
         state = np.float32(observation)
         states.append(state)
         dist = actor_network(Variable(torch.Tensor(state)))
-        print("mean", dist.loc)
-        print("std",dist.scale)
         action = dist.sample()
         entropy += dist.entropy().mean()
         action = action.cpu().numpy()
-        new_observation,reward,done,_ = env.step(action)
+        new_observation,reward,done,_ = envs.step(action)
         episode_reward += reward
         actions.append(action)
         rewards.append(reward)
         observation = new_observation
-        if done:
-            break
-    print ('REWARDS :- ', episode_reward)
+    #print ('REWARDS :- ', episode_reward)
     return states,actions,rewards,entropy
 
 def discount_reward(r, gamma):
@@ -96,19 +121,30 @@ def update_network(states, actions, rewards, entropy):
     dist = actor_network(states_var)
     log_probs = dist.log_prob(actions_var)
     # calculate qs
-    rewards = Variable(torch.Tensor(discount_reward(rewards,0.99)))
+    rewards = Variable(torch.Tensor(discount_reward(rewards,0.99))).view(-1,1)
     rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
-    actor_network_loss = - torch.mean(torch.sum(log_probs * rewards))
+    actor_network_loss = - torch.mean(torch.sum(log_probs * rewards)) - 0.001*entropy
     #print("loss",actor_network_loss)
     actor_network_loss.backward()
     actor_network_optim.step()
 
+
 MAX_EPISODES = 5000
-MAX_STEPS = 1000
+_ep = 0
+early_stop = False
+test_rewards = []
+threshold_reward = -200
 
-
-for _ep in range(MAX_EPISODES):
+while _ep < MAX_EPISODES and not early_stop:
     observation = env.reset()
-    print ('EPISODE :- ', _ep)
+    #print ('EPISODE :- ', _ep)
     states,actions,rewards,entropy = roll_out(SAMPLE_NUMS)
     update_network(states,actions,rewards,entropy)
+
+    if _ep % 100 == 0:
+        test_reward = np.mean([test_env() for _ in range(10)])
+        test_rewards.append(test_reward)
+        print ('EPISODE :- ', _ep)
+        print("TEST REWARD :- ", test_reward)
+        if test_reward > threshold_reward: early_stop = True
+    _ep += 1
